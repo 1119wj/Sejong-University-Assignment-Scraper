57bace9fefb8c78761130c0959bc323622c6709b
1119wj <1119wj@naver.com>
2024-03-08 11:48:30 +0900

    Feat: MUI select효과 제거

--- src/components/CourseTabs.tsx
+++ src/components/CourseTabs.tsx
@@ -2,22 +2,41 @@
 
 
 import { Box, Tab, Tabs } from '@mui/material'
-import type { Course } from '@src/types'
 
+import type { Course } from '@src/types'
+import { courseData } from '@src/data/data'
 type Props = {
     courseList: Course[]
     selectedCourseId: string
     setSelectedCourseId: React.Dispatch<React.SetStateAction<string>>
   }
-  
+
   const CourseTabs = ({ courseList, selectedCourseId, setSelectedCourseId }: Props) => {
     console.log(selectedCourseId);
     return (
-        <Box display="flex" sx={{height:400, width:'40%'}}>
+        <Box display="flex" sx={{width:'40%',height:'90%'}}>
         <Tabs
         orientation="vertical">
         {courseList.map((course, index) => (
-          <Tab label={course.title} tabIndex={index} key={index} onClick={()=>setSelectedCourseId(course.id)} />
+          <Tab label={course.title} sx={
+            {
+              fontSize:'14px',
+              "&:hover":{fontWeight:'600', color:'#363535'},
+              "&:focus":{
+                outline:'none',
+                border:'none'
+              },
+              "&.Mui-selected": {
+                outline: 'none',
+                border: 'none',                                                                   
+              },
+              fontWeight: selectedCourseId === course.id ? '600':'400',
+              color: selectedCourseId === course.id ? '#f7f7f6':'#797979',
+              bgcolor: selectedCourseId === course.id ? '#b62724':'bg.color',
+              transition: 'color 0.2s,bgcolor 0.2s',
+              borderRadius : 2,
+              cursor:'pointer',
+            }} tabIndex={index} key={index} onClick={()=>setSelectedCourseId(course.id)} />
         ))}
       </Tabs>
       </Box>
